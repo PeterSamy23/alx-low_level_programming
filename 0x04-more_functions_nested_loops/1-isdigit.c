30224926d5b5979b2769e9022333b07a6740f335
PeterSamy23 <petersamy23@gmail.com>
2023-07-22 17:43:36 -0700

    Tasks

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * _isdigit - checks if input is digit between 0 - 9
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,4 @@
-#include <main/h>
+#include "main/h"
 
 /** mul - multiple two integers
  *
